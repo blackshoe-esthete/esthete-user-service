0425094b5dcc72acea63721de38bbf91f79a80cf
jaehui327 <jaeheekim970327@gmail.com>
2024-03-09 15:28:48 +0900

    Init: esthete user service

---
+++ src/main/java/com/blackshoe/esthete/EstheteUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blackshoe.esthete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstheteUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstheteUserServiceApplication.class, args);
+	}
+
+}
