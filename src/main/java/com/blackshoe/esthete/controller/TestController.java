55787893f4609c8085178f45cc6c678f77a6d083
Subin <70842920+bin-pro@users.noreply.github.com>
2024-06-21 11:39:00 +0900

    Chore: add gateway test

--- src/main/java/com/blackshoe/esthete/controller/TestController.java
+++ src/main/java/com/blackshoe/esthete/controller/TestController.java
@@ -1,19 +1,32 @@
 package com.blackshoe.esthete.controller;
 
+import com.blackshoe.esthete.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/test")
 public class TestController {
-    @GetMapping("/test")
+    @GetMapping()
     public ResponseEntity<?> Test() {
 
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
 
         return ResponseEntity.ok("Login user : " + name + "!");
+    }
+    @GetMapping("/gateway")
+    public ResponseEntity<ResponseDto> test() {
+
+        Map<String, String> map = Map.of("test", "test");
+        ResponseDto responseDto = ResponseDto.builder().payload(map).build();
+
+        return ResponseEntity.ok().body(responseDto);
     }
 }
