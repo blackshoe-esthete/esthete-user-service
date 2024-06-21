8f39d53d8722ce8d4b767c5a50c691e5d5b91bad
Subin <70842920+bin-pro@users.noreply.github.com>
2024-06-21 20:25:48 +0900

    Chore: add kafka

--- src/main/java/com/blackshoe/esthete/config/KafkaConfig.java
+++ src/main/java/com/blackshoe/esthete/config/KafkaConfig.java
@@ -1,7 +1,9 @@
 package com.blackshoe.esthete.config;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
+
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.OffsetResetStrategy;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -9,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ContainerProperties;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -19,7 +22,6 @@ public class KafkaConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String BOOTSTRAP_SERVERS;
 
-    // producer configuration
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
@@ -36,15 +38,16 @@ public class KafkaConfig {
         return new KafkaTemplate<>(producerFactory());
     }
 
-    // consumer configuration
     @Bean
     public ConsumerFactory<String, String> consumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
 
         configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "user-service");
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "esthete-user-service");
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, OffsetResetStrategy.EARLIEST.name().toLowerCase());
+
 
         return new DefaultKafkaConsumerFactory<>(configProps);
     }
@@ -54,6 +57,7 @@ public class KafkaConfig {
         ConcurrentKafkaListenerContainerFactory<String, String>
                 kafkaListenerContainerFactory = new ConcurrentKafkaListenerContainerFactory<>();
         kafkaListenerContainerFactory.setConsumerFactory(consumerFactory());
+        kafkaListenerContainerFactory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
         return kafkaListenerContainerFactory;
     }
 }
