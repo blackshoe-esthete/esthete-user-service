406a372803b1ef9d487b59aabd34ed31b789a2dd
BoyoungH <hyong0903@naver.com>
2024-08-04 00:01:38 +0900

    Feat: social-login-signup api위한 saveUserInSecuritContext 함수 추가하기

--- src/main/java/com/blackshoe/esthete/service/SecurityService.java
+++ src/main/java/com/blackshoe/esthete/service/SecurityService.java
@@ -6,7 +6,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Map;
 
 public interface SecurityService {
-    Map<String, String> saveUserInSecurityContext(OAuth2Dto.OAuth2RequestDto requestDto);
+    Map<String, String> saveUserInSecurityContext(OAuth2Dto.OAuth2CheckDto requestDto);
+    Map<String, String> saveUserInSecurityContextForOAuthSignUp(OAuth2Dto.OAuth2SignUpRequestDto requestDto);
     Map<String, String> saveUserInSecurityContext(String socialId, String socialProvider);
     UserDetails loadUserBySocialIdAndSocialProvider(String socialId, String socialProvider);
 
